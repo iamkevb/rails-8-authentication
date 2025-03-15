99255afed0bae44f0bfe72ac1249249b7ef3f722
Kevin Barnes <iamkevb@gmail.com>
2025-03-15 10:47:38 -0400

    register new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resources :users, only: [ :new, :create ]
   root "home#index"
   get "/protected", to: "protected#show"
 end
